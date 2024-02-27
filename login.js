455b6120bfee00ecd421217d2de9125bdbd56f24
Charles Oliphant <coli2001@byu.edu>
2024-02-27 15:47:26 -0700

    well i worked on this for an hour, changed some things

--- login.js
+++ login.js
@@ -1,7 +1,10 @@
-localStorage.setItem("index", 0)
+if (!localStorage.getItem("index")){
+    localStorage.setItem("index", 0)
+}
 
 function storename(){
-    const currentindex = localStorage.getItem("index") + 1;
+    const currentindex = localStorage.getItem("index");
     const idname = document.querySelector("#name");
     localStorage.setItem(currentindex, idname.value);
+    localStorage.setItem("index", currentindex+1)
 };  
--- question.js
+++ question.js
@@ -1,5 +1,22 @@
 function storequestion () {
-    let index = localStorage.getItem("index")
-    const idname = document.querySelector("#question-submit");
-    localStorage.setItem(index, idname.value)
+    let index = localStorage.getItem("index");
+    let subject_question = localStorage.getItem(index);
+    subject_question.append(document.querySelector("#subject").value, document.querySelector("#question").value);
+
+    localStorage.setItem(index, subject_question);
 }
+
+function saveQuestion(score) {
+    let index = localStorage.getItem("index");
+    let name = [localStorage.getItem(index)];
+    name.append(document.querySelector("#subject").value, document.querySelector("#question").value);
+
+
+    let questions = [];
+    const questionText = localStorage.getItem('questions');
+    if (questionText) {
+      questions = JSON.parse(questionText);
+    }
+    
+    localStorage.setItem('questions', JSON.stringify(scores));
+  }
