c85976492f2d95087867c1fe81c5372284d962c2
Charles Oliphant <coli2001@byu.edu>
2024-02-28 14:36:08 -0700

    creating a function to make and update table

--- queue.js
+++ queue.js
@@ -3,6 +3,33 @@ function getquestion() {
     // let question = JSON.parse(localStorage.getItem(name))["question"];
     // let subject = JSON.parse(localStorage.getItem(name))["subject"];
 
-    const tableElement = que
+    let queue = [];
+    const queueMap = localStorage.getItem("queue");
+    if (queue){
+        queue = JSON.parse(queueMap);
+    }
+    
+    const tableElement = document.querySelector("#queue");
+
+    if (queue.length){
+        for (let [name, map] of queue){
+            const nameEL = document.createElement("td");
+            const subjectEL = document.createElement("td");
+            const questionEL = document.createElement("td");
+            const acceptEL = document.createElement("td");
+
+            nameEL.textContent = name;
+            subjectEL.textContent = map.subject;
+            questionEL.textContent = map.question;
+            acceptEL.textContent = "temp";
+
+            const rowEL = document.createElement("tr");
+
+
+
+        }
+    }
+
+
 
 }
