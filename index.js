096a6c5bc102bfb2a2fd6d144dd6e39185bf1884
Charles Oliphant <coli2001@byu.edu>
2024-03-08 22:59:49 -0700

    got rid of the bootstrap for the table, made my own

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ apiRouter.delete('/queue', (req, res) => {
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: __dirname+'\\public' });
-  console.log("it works")
 });
 
 app.listen(port, () => {
