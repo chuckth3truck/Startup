dfb33ba32aad0c75da06767f8a0a7b2dc615e71d
Charles Oliphant <coli2001@byu.edu>
2024-04-08 20:58:53 -0600

    created login, question, queue components

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,47 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Question } from './question/question';
+import { Queue } from './queue/queue';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+        <div className="body">
+    <header>
+        <h1>THE ULTIMATE HELP QUEUE</h1>
+        <nav>
+            
+            <div className="menu-items">
+                <NavLink className='nav-link' to='question'>Ask A Question</NavLink>
+                <NavLink className='nav-link' to='queue'>Queue</NavLink>
+                <button type="submit" onclick="logout()">Logout</button>
+            </div>
+        </nav>
+    </header>
+
+    <Routes>
+    <Route path='/' element={<Login />} />
+    <Route path='/question' element={<Question />} />
+    <Route path='/queue' element={<Queue />} />
+
+    </Routes>
+
+    <footer>
+        <div className="name">Charles Oliphant</div>
+        <div><a href="https://github.com/chuckth3truck/Startup">GitHub</a></div>
+    </footer>
+    </div>
+    </BrowserRouter>
+
+  );
 }
+
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+  }
+
+export default App;
---
+++ src/question/question.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Question() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>question displayed here</div>
+    </main>
+  );
+}
